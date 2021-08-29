004598f944ab7e5e6759f6941fea6a08998dac3c
JeffWang0325 <Jeffbaseball455172@gmail.com>
2021-08-29 15:43:33 +0800

    add 深度學習模型優化與梯度下降法 (new)

---
+++ 深度學習模型優化與梯度下降法 (new)/code/ShowMnistImage.py
@@ -0,0 +1,20 @@
+
+# 載入 MNIST 資料庫的訓練資料，並自動分為『訓練組』及『測試組』
+import os
+from keras.datasets import mnist
+#data_folder = os.path.join(os.getcwd(), "datasets/mnist.npz")
+(X_train, y_train), (X_test, y_test) = mnist.load_data() #path=data_folder)
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(10):
+    img = X_train[y_train == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+# plt.savefig('images/12_5.png', dpi=300)
+plt.show()
---
+++ 深度學習模型優化與梯度下降法 (new)/code/ShowSingleDigitImage.py
@@ -0,0 +1,46 @@
+
+# 載入 MNIST 資料庫的訓練資料，並自動分為『訓練組』及『測試組』
+import os
+import sys
+
+if len(sys.argv) <= 1:
+    print("需輸入參數 0~9")
+    sys.exit()
+#print(sys.argv[1])
+    
+if len(sys.argv[1].strip()) != 1:
+    print("需輸入參數 0~9")
+    sys.exit()
+try: 
+    no = int(sys.argv[1].strip())
+except:
+    print("需輸入參數 0~9")
+    sys.exit()
+    
+
+from keras.datasets import mnist
+data_folder = os.path.join(os.getcwd(), "../datasets/mnist.npz")
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+for i in range(10):
+    img = X_train[y_train == no][i].reshape(28, 28)
+    
+    # show pixels' value
+    for j in range(28):
+        pixels = ""
+        for k in range(28):
+            pixels += str(img[j,k]).zfill(3) + " "
+        print(pixels)
+    print("")
+
+    ax[i].imshow(img, cmap='Greys')
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+# plt.savefig('images/12_5.png', dpi=300)
+plt.show()
